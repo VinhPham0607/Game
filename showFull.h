5142708fdd9783887872395434bc4e6ab7a03be1
VinhPham0607 <90959311+VinhPham0607@users.noreply.github.com>
2024-08-02 09:44:28 +0700

    add

--- showFull.h
+++ showFull.h
@@ -5,5 +5,9 @@ void showStartMenu(SDL_Renderer *gRenderer, SDL_Texture *gStartMenu);
 void showFullTable(SDL_Renderer *gRenderer, SDL_Texture *gTextureBox, SDL_Texture *gTextureGalaxy, int *arr, int n);
 void showBombTable(SDL_Renderer *gRenderer, SDL_Texture *gTextureBox, SDL_Texture *gTextureGalaxy, SDL_Texture *gTextureMar,
                    int *arr, int n, int *locatBombX, int *locatBombY, int bomb);
+void showSwap(SDL_Renderer *gRenderer, SDL_Texture *gTextureBox, SDL_Texture *gTextureGalaxy,
+              int *arr, int n, int x, int y, int direction, int timewait);
+void showResult(SDL_Renderer *gRenderer, SDL_Texture *gTextureBox, SDL_Texture *gTextureGalaxy, SDL_Texture *gTextureMar, SDL_Texture *gTextureLevelup,
+                int *arr, int n, int *locatBombX, int *locatBombY, int bomb, int result, int tmp, int timewait);
 void showWinner(SDL_Renderer *gRenderer, SDL_Texture *gTextureCongrats);
 #endif // SHOWFULL_H
